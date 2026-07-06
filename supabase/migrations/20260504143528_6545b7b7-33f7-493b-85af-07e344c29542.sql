b1340ed469a45c67bf4dcb9f52d31aa8434a677e
itecandcompany <itecandcompany@gmail.com>
2026-07-06 12:50:04 +0000

    good

--- supabase/migrations/20260504143528_6545b7b7-33f7-493b-85af-07e344c29542.sql
+++ supabase/migrations/20260504143528_6545b7b7-33f7-493b-85af-07e344c29542.sql
@@ -1,4 +1,4 @@
-ALTER TABLE public.job_locations REPLICA IDENTITY FULL;
+yes ALTER TABLE public.job_locations REPLICA IDENTITY FULL;
 DO $$ BEGIN
   ALTER PUBLICATION supabase_realtime ADD TABLE public.job_locations;
 EXCEPTION WHEN duplicate_object THEN NULL; END $$;
